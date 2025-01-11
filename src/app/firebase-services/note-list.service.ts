97c1c1e7d938d21e850476bdd9f29108ab74d517
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2025-01-11 14:02:51 +0100

    Praxis: READ -> onSnapshot(), collectionData()

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -11,39 +11,48 @@ export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
 
-  items$;
-  items;
-
-  unsubList;
-  unsubSingle;
+  unsubTrash;
+  unsubNotes;
 
   firestore: Firestore = inject(Firestore);
 
   constructor() {
-
-    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
-      list.forEach(element => {
-        console.log(element);
-      });
-    });
-
-    this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "ahartzre5457"), (element) => {
-      
-    });
-
-    this.unsubSingle();
-    this.unsubList();
-
-    this.items$ = collectionData(this.getNotesRef());
-    this.items = this.items$.subscribe( (list) => {
-      list.forEach(element => {
-        console.log(element);
-      });
-    })
-    this.items$.unsubscribe();
+    this.unsubNotes = this.subNotesList();
+    this.unsubTrash = this.subTrashList();
   }
 
-  // const itemCollection = collection(this.firestore, 'items');
+  ngonDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || "note",
+      title: obj.title || "",
+      content: obj.content || "",
+      marked: obj.marked || false,
+    }
+  }
 
   getNotesRef() {
     return collection(this.firestore, 'notes');
